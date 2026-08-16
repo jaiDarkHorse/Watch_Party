f8150815f7eb9f8bcb2e974511e72bb6b387c877
jai <jeyasurya2005@gmail.com>
2026-08-16 13:50:17 +0530

    Reinitialize with correct metadata

---
+++ SyncWatchApplication/src/main/java/com/syncwatch/SyncWatchApplication/SyncWatchApplication.java
@@ -0,0 +1,13 @@
+package com.syncwatch.SyncWatchApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncWatchApplication.class, args);
+	}
+
+}
---
+++ SyncWatchApplication/src/test/java/com/syncwatch/SyncWatchApplication/SyncWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncwatch.SyncWatchApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
